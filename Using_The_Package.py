e94337914cb545aafb430e72d06041e102e5d012
abhishek453 <abhishek.alagarasu@gmail.com>
2022-12-30 21:09:47 +0530

    Importing and using our package

---
+++ Using_The_Package.py
@@ -0,0 +1,28 @@
+import numpy as np
+import h5py
+from LogisticRegression import Model_Binary_Classification as LR
+    
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+
+train_set_x = train_set_x_flatten / 255
+test_set_x = test_set_x_flatten / 255
+
+model = LR(X_train = train_set_x, Y_train=train_set_y, X_test=test_set_x, Y_test=test_set_y,num_iterations=1000,learning_rate=0.05,print_cost=True)
